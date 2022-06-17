c30c83d682cfbc8107e6482f42ae04b70c2a5607
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-17 09:55:37 -0400

    Added file: [index.php]

---
+++ index.php
@@ -0,0 +1,20 @@
+<!--
+#############################################################################
+##		index.php
+#############################################################################
+--> 
+
+<link rel="stylesheet" href="FOIL-UI.css" type="text/css">
+<link rel="stylesheet" href="config.css" type="text/css">
+
+<div class="FOIL-Font-Styles--sans-serif">
+
+<div class="FOIL-Navigation-Bar">
+<h1> System Information </h1> 
+</div>
+
+
+
+
+
+</div> 
